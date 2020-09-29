53eecf7f71d7c20849dfc058751925dd71ff8ab8
5baddi <project@baddi.info>
2020-09-29 10:57:47 +0100

    Update routing

--- routes/api.php
+++ routes/api.php
@@ -15,19 +15,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    // Get User
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Logout action
+    Route::post('/logout', 'AuthenticationController@logout');
 });
 
+// API V1 routes
 Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1'], function(){
     // Brands
     Route::get('/brands', 'BrandController@index');
     Route::post('/brands', 'BrandController@create');
     Route::put('/brands/{brand}', 'BrandController@update');
+    Route::delete('/brands/{brand}', 'BrandController@destroy');
     // Route::get('/brand/{brand}/trackers', 'TrackerController@fetchByBrand');
 });
 
-Route::middleware('auth:sanctum')->post('/logout', 'AuthenticationController@logout');
 // Route::middleware('auth:sanctum')->post('/register', 'UserController@register');
 
 // Route::middleware('auth:sanctum')->get('/brands', 'BrandController@index');
