ae318c1917dff5326647104905e659bfe69c6fee
5baddi <project@baddi.info>
2020-10-16 11:17:47 +0100

    Update: UI

---
+++ resources/js/components/modals/ConfirmationModal.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="modal">
+      <div class="modal-content">
+         <header>
+            <h4 class="heading">{{ title }}</h4>
+         </header>
+         <div class="modal-form">
+            <div class="modal-form__actions">
+                <!-- <button class="btn btn-success" @click.prevent="submit" type="submit">{{ brand.id ? "Update" : "Create" }}</button> -->
+                <!-- <button class="btn btn-danger" @click.prevent="dismiss" type="button">Cancel</button> -->
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+<script>
+export default {
+    props: {
+        title:{
+            type: String,
+            default: "Are you sure?"
+        }
+    },
+}
+</script>
