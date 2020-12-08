bb9d988d8442e92f1a2e130a3d455d34cdf16be9
5baddi <project@baddi.info>
2020-12-08 18:28:58 +0100

    Remove test

--- app/Console/Commands/InstagramCommand.php
+++ app/Console/Commands/InstagramCommand.php
@@ -83,7 +83,6 @@ class InstagramCommand extends Command
         // Scrap each influencer details
         foreach($influencers as $influencer){
             try{
-                dd($influencer->posts()->count(), $influencer->medias);
                 // Ignore last updated influencers
                 if($influencer->posts()->count() === $influencer->medias)
                     continue;
