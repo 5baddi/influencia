1df82afdb8643f64abb320b611240cc9fec1eb57
5baddi <project@baddi.info>
2021-01-01 20:49:24 +0100

    Publish version 0.4.0

--- config/scraper.php
+++ config/scraper.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version'   =>  '0.3.9',
+    'version'   =>  '0.4.0',
     'support'   =>  env('SUPPORT_EMAIL'),
     'usd2eur'   =>  env('USD2EUR'),
     'fbcost_perimpressions'    =>  env('FBCOSTPERIMPRESSIONS'),
