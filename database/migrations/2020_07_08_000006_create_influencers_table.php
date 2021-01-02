17030db067a0007ff60a19a122980ee96085f99a
5baddi <project@baddi.info>
2021-01-02 14:00:51 +0100

    Optimize migrations

--- database/migrations/2020_07_08_000006_create_influencers_table.php
+++ database/migrations/2020_07_08_000006_create_influencers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInfluencerPosts extends Migration
+class CreateInfluencersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,37 @@ class CreateInfluencerPosts extends Migration
      */
     public function up()
     {
+        // Influencers
+        Schema::create('influencers', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique()->nullable(false);
+            $table->enum('platform', ['instagram', 'snapchat', 'youtube'])->default('instagram');
+            $table->string('account_id')->nullable(false);
+            $table->string('username')->nullable(false);
+            $table->string('name')->nullable();
+            $table->longText('biography')->nullable();
+            $table->string('website')->nullable();
+            $table->text('pic_url')->nullable();
+            $table->float('engagement_rate')->nullable();
+            $table->integer('followers')->nullable();
+            $table->integer('follows')->nullable();
+            $table->integer('medias')->nullable();
+            $table->integer('highlight_reel')->nullable();
+            $table->string('business_category')->nullable();
+            $table->string('business_email')->nullable();
+            $table->string('business_phone')->nullable();
+            $table->json('business_address')->nullable();
+            $table->boolean('is_business')->nullable();
+            $table->boolean('is_private')->nullable();
+            $table->boolean('is_verified')->nullable();
+            $table->boolean('banned')->default(false);
+            $table->integer('video_views')->nullable();
+            $table->string('country_code')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+
+        // Influencer posts
         Schema::create('influencer_posts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('influencer_id');
@@ -69,5 +100,6 @@ class CreateInfluencerPosts extends Migration
     public function down()
     {
         Schema::dropIfExists('influencer_posts');
+        Schema::dropIfExists('influencers');
     }
 }
